0b9911decc48cff7a3990662f63c775ce1e55e07
Young <younghotkim@gmail.com>
2021-05-26 10:32:56 +0900

    chapter01

--- src/com/javaex/ex03/Ex04.java
+++ src/com/javaex/ex03/Ex04.java
@@ -13,11 +13,11 @@ public class Ex04 {
 		int t = sc.nextInt();
 		
 		if(t<=8) {
-			System.out.println("10000원");
+			System.out.print("임금은 " + t*10000 + "원 입니다.");
 		}
 		
 		else {
-			System.out.println("12000원");
+			System.out.println("임금은" + (80000+(t-8)*12000) + "원 입니다.");
 		}
 		
 		sc.close();
